6a241c069770e68fbb9bbcce1a2ee6ae7591374d
MivikSihag <m_sihag@mt.iitr.ac.in>
2024-07-15 13:08:41 +0530

    changed sidebar names

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,23 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return (
         <div className="bg-bcgClr w-64 min-h-screen p-4 text-white flex flex-col">
             <h2 className="text-2xl text-center mb-4">Information</h2>
-            <ul className="flex-1">
-                <li className="mb-2">
-                    <a href="#" className="block px-4 py-2 text-center rounded hover:bg-white hover:text-bcgClr">Option 1</a>
-                </li>
-                <li className="mb-2">
-                    <a href="#" className="block px-4 py-2 text-center rounded hover:bg-white hover:text-bcgClr">Option 2</a>
-                </li>
-                <li className="mb-2">
-                    <a href="#" className="block px-4 py-2 text-center rounded hover:bg-white hover:text-bcgClr">Option 3</a>
-                </li>
-                <li className="mb-2">
-                    <a href="#" className="block px-4 py-2 text-center rounded hover:bg-white hover:text-bcgClr">Invoice Match</a>
-                </li>
-            </ul>
+            <div className="flex-1">
+            <Link to={"/clients"}>
+                <div className="mb-2 cursor-pointer">
+                    <p className="block px-4 py-2 text-center text-white rounded hover:bg-white hover:text-bcgClr">Executive Summary</p>
+                </div>
+            </Link>
+                <Link to={"/tablepage"}>
+                <div className="mb-2 cursor-pointer">
+                    <p className="block px-4 py-2 text-center text-white border-0 rounded hover:bg-white hover:text-bcgClr">Invoice Match</p>
+                </div>
+                </Link>
+            </div>
         </div>
     );
 };
