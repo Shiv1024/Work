331b8e76d083e5594f0cfae16cb5be5a30d38948
MivikSihag <m_sihag@mt.iitr.ac.in>
2024-07-17 15:45:49 +0530

    added pagination

--- src/Components/DiffTables.js
+++ src/Components/DiffTables.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import TablePagination from '@mui/material/TablePagination';
 
 function DiffTable (props)
 {
@@ -9,6 +10,18 @@ function DiffTable (props)
     setIsDiffTableVisible(!isDiffTableVisible);
   };
 
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
     <div className="border border-gray-300 rounded-md shadow-sm w-full flex flex-col mb-4">
     <div className="bg-bgClr2 p-4 flex justify-between items-center rounded-t-md cursor-pointer " onClick={toggleDiffTableVisibility}>
@@ -16,7 +29,7 @@ function DiffTable (props)
       <ArrowDropDownIcon className={`transition-transform transform ${isDiffTableVisible ? 'rotate-180' : 'rotate-0'}`} />
     </div>
     {isDiffTableVisible && (
-      <div className="flex flex-col lg:flex-row justify-between p-4 flex-grow">
+      <div className="flex flex-col justify-between p-4">
         <div className="w-full">
           <table className="my-auto border-collapse border border-slate-400 w-full">
             <thead>
@@ -29,7 +42,7 @@ function DiffTable (props)
               </tr>
             </thead>
             <tbody>
-              {props.Data.map((row, index) => (
+              {props.Data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => (
                 <tr key={index}>
                   <td className="py-3 px-4 bg-bgClr3 border-b border-gray-200 text-center">
                     {row.inclusions}
@@ -50,6 +63,20 @@ function DiffTable (props)
               ))}
             </tbody>
           </table>
+        </div>
+
+        <div className="bg-white w-full flex justify-start">
+          <div className="w-auto">
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={props.Data.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </div>
         </div>
       </div>
     )}
